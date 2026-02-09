4848cf5b43fbcec60c723a27032d5bcdf68ea191
rayenMrebai <rayenmribai1@gmail.com>
2026-02-09 19:23:32 +0100

    Test add user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,48 @@
 package org.example;
 
+import org.example.services.salaire.BonusRuleService;
+import org.example.services.salaire.SalaireService;
 import org.example.util.DatabaseConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DatabaseConnection
-                .getInstance()
-                .getConnection();
+        Connection conn = DatabaseConnection.getInstance().getConnection();
+        SalaireService salaireService = new SalaireService();
+        BonusRuleService bonusRuleService = new BonusRuleService();
+
+        // test add usr
+
+        /*String sqlUser = "INSERT INTO useraccount(name, role, email) VALUES (?, ?, ?)";
+
+        // Ajouter un MANAGER
+        try (PreparedStatement ps = conn.prepareStatement(sqlUser)) {
+            ps.setString(1, "Mohamed Manager");
+            ps.setString(2, "MANAGER");
+            ps.setString(3, "Mohamed.manager@mail.com");
+            ps.executeUpdate();
+            System.out.println("✅ Utilisateur MANAGER ajouté avec succès");
+        } catch (Exception e) {
+            System.out.println("Erreur insertion MANAGER : " + e.getMessage());
+        }
+
+        // Ajouter un EMPLOYE
+        try (PreparedStatement ps = conn.prepareStatement(sqlUser)) {
+            ps.setString(1, "Rayen Employe");
+            ps.setString(2, "EMPLOYE");
+            ps.setString(3, "rayen.employe@mail.com");
+            ps.executeUpdate();
+            System.out.println("✅ Utilisateur EMPLOYE ajouté avec succès");
+        } catch (Exception e) {
+            System.out.println("Erreur insertion EMPLOYE : " + e.getMessage());
+        }*/
+
+
+
 
     }
 }
