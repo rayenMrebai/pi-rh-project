1d56aac36228ea074dc860417d6ead9070f90d3c
Sara <sarabeji123@gmail.com>
2026-02-11 21:24:28 +0100

    ajout des class skill et TrainingProgram

--- src/main/java/org/example/model/formation/Skill.java
+++ src/main/java/org/example/model/formation/Skill.java
@@ -1,4 +1,83 @@
 package org.example.model.formation;
 
+import java.security.PrivateKey;
+
 public class Skill {
+    //Attributs
+    private int id;
+    private String nom;
+    private String description;
+    private  String categorie ;
+    private int levelRequired;
+    //constructeurs
+    public Skill() {}
+    public Skill(String nom, String description, String categorie) {
+        this.nom = nom;
+        this.description = description;
+        this.categorie = categorie;
+    }
+    public Skill(int id, String nom, String description, String categorie, int levelRequired) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.categorie = categorie;
+        this.levelRequired = levelRequired;
+    }
+    //getters
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public int getLevelRequired() {
+        return levelRequired;
+    }
+    //setters
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public void setLevelRequired(int levelRequired) {
+        this.levelRequired = levelRequired;
+    }
+    //toString
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "id=" + id +
+                ", name='" + nom + '\'' +
+                ", description='" + description + '\'' +
+                ", categorie='" + categorie + '\'' +
+                ", levelRequired=" + levelRequired +
+                '}';
+    }
 }
+
+
